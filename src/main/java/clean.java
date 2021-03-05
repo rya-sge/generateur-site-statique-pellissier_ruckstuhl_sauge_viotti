889d8621fea0083b240c7932e3be5734423d7b88
Nico <n.viotti@hotmail.com>
2021-03-05 12:40:01 +0100

    Added picocli Hello World

--- src/main/java/clean.java
+++ src/main/java/clean.java
@@ -1,6 +1,13 @@
-public class clean {
-    void print()
-    {
-        System.out.println(this.getClass() + ".print()");
-    }
+import picocli.CommandLine;
+
+class HelloWorldCommand implements Runnable {
+    public static void main(String[] args) {
+        CommandLine.run(new HelloWorldCommand(), args);
+        }
+
+        @Override
+        public void run() {
+            System.out.println("Hello World!");
+        }
 }
+
