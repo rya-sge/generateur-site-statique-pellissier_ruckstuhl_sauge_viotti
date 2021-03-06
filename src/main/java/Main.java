fd8f363e34ddf35bcef7adde1ce46029781ca45c
david-pellissier <dav.pellissier@gmail.com>
2021-03-06 10:32:31 +0100

    Modif Main + commande New

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,21 @@
+import picocli.CommandLine;
 
-public class Main {
+@CommandLine.Command(
+        name="lab02",
+        version = "v. lab02 GEN",
+        description = "Interdum et malesuada fames ac ante ipsum primis in faucibus. ",
+        mixinStandardHelpOptions = true,
+        subcommands={NewCmd.class}
+)
+public class Main implements Runnable {
     public static void main(String[] args ){
-        System.out.println("Hello, world ");
+        int exitCode = new CommandLine(new Main()).execute(args);
+        System.exit(exitCode);
     }
 
+    @Override
+    public void run(){
+        System.out.println("Hello, world ");
+
+    }
 }
