aaad47f0c087e1161bb0332a13b4e571a9cddec0
rya-sge <ryan.sauge@heig-vd.ch>
2021-03-14 10:39:45 +0100

    read and write json, correction

--- src/main/java/Read.java
+++ src/main/java/Read.java
@@ -1,47 +1,58 @@
-// inspiré de
-// https://examples.javacodegeeks.com/core-java/xml/jdom/read-xml-file-in-java-using-jdom-parser-example/
+// sources : https://howtodoinjava.com/json/json-simple-read-write-json-examples/
 
-import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
-import java.util.List;
 
-import org.jdom2.*;
-import org.jdom2.input.*;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
-public class Read {
-
-    private static final String xmlFilePath = "src/menu.xml";
+public class Application {
 
     public static void main(String[] args) {
 
-        SAXBuilder saxBuilder = new SAXBuilder();
-        File xmlFile = new File(xmlFilePath);
+        //JSON parser object pour lire le fichier
+        JSONParser jsonParser = new JSONParser();
 
-        try {
-            Document document = (Document) saxBuilder.build(xmlFile);
-            Element rootElement = document.getRootElement();
+        try (FileReader reader = new FileReader("src/jsonFile.json")) {
 
-            List listMenu = rootElement.getChildren("menu");
-            for (int iMenu = 0; iMenu < listMenu.size(); ++iMenu) {
+            // lecture du fichier
+            Object obj = jsonParser.parse(reader);
 
-                Element menu = (Element) listMenu.get(iMenu);
-                System.out.println("nom    : "  + menu.getChildText("nom"));
-                System.out.println("pos    : "  + menu.getAttributeValue("position"));
+            JSONArray personne = (JSONArray) obj;
+            System.out.println(personne);
 
-                List listOption = menu.getChildren("option");
-                for (int iOption = 0; iOption < listOption.size(); ++iOption) {
-                    Element option = (Element) listOption.get(iOption);
-                    System.out.println("option : "  + option.getText());
-                }
-            }
+            // parcours du tableau de personnes
+            personne.forEach(pers->parsePersonneObject((JSONObject)pers));
+
         }
 
-        catch (IOException io) {
-            System.out.println(io.getMessage());
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
         }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
 
-        catch (JDOMException jdomex) {
-            System.out.println(jdomex.getMessage());
-        }
+    private static void parsePersonneObject(JSONObject pers) {
+
+        // Obtenir l'objet personne dans la liste
+        JSONObject persObject = (JSONObject) pers.get("personne");
+
+        // obtenir les détails ...
+        String nom    = (String) persObject.get("nom");
+        String prenom = (String) persObject.get("prenom");
+        String email  = (String) persObject.get("email");
+
+        // afficher le contenu
+        System.out.println(nom);
+        System.out.println(prenom);
+        System.out.println(email);
     }
 }
--- src/main/java/Write.java
+++ src/main/java/Write.java
@@ -1,96 +1,45 @@
-// sources : https://openclassrooms.com/fr/courses/1766341-structurez-vos-donnees-avec-xml/1768662-dom-exemple-dutilisation-en-java
+import java.io.FileWriter;
+import java.io.IOException;
 
-// 0. imports nécessaires
-//import java.io.*;
-//import org.w3c.dom.*;
-//import javax.xml.parsers.*;
-//import javax.xml.transform.*;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 
-import java.io.File;
+public class Application
+{
+    @SuppressWarnings("unchecked")
+    public static void main( String[] args )
+    {
+        // Charlie Chaplin
+        JSONObject Charlie = new JSONObject();
+        Charlie.put("prenom", "Charlie");
+        Charlie.put("nom",    "Chaplin");
+        Charlie.put("eamil",  "charlie@chapelin.fun");
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.OutputKeys;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerConfigurationException;
-import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
-
-import org.w3c.dom.Comment;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-
-public class Write{
-    public static void main(final String[] args) {
-
-        // Etape 1 : récupération d'une instance de la classe "DocumentBuilderFactory"
-        final DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-
-        try {
-            // Etape 2 : création d'un parseur
-            final DocumentBuilder builder = factory.newDocumentBuilder();
-
-            // Etape 3 : création d'un Document
-            final Document document = builder.newDocument();
-
-            // Etape 4 : création de l'Element racine
-            final Element allMenus = document.createElement("menus");
-            document.appendChild(allMenus);
-
-            // Etape 5 : création d'un menu
-            final Comment commentaire = document.createComment("Fichier");
-            allMenus.appendChild(commentaire);
-
-            final Element menu = document.createElement("menu");
-            menu.setAttribute("position", "1");
-            allMenus.appendChild(menu);
-
-            final Element nom = document.createElement("nom");
-            nom.appendChild(document.createTextNode("Fichier"));
-            menu.appendChild(nom);
-
-            // Etape 6 : création des options
-            final Element nouveau = document.createElement("option");
-            nouveau.appendChild(document.createTextNode("Nouveau"));
-            menu.appendChild(nouveau);
-
-            final Element sauver = document.createElement("option");
-            sauver.appendChild(document.createTextNode("Sauver"));
-            menu.appendChild(sauver);
-
-            final Element quitter = document.createElement("option");
-            quitter.appendChild(document.createTextNode("Quitter"));
-            menu.appendChild(quitter);
+        JSONObject CharlieObject = new JSONObject();
+        CharlieObject.put("personne", Charlie);
 
 
-            // Etape 7 : finalisation
-            final TransformerFactory transformerFactory = TransformerFactory.newInstance();
-            final Transformer transformer = transformerFactory.newTransformer();
-            final DOMSource source        = new DOMSource(document);
-            final StreamResult sortie     = new StreamResult(new File("src/file.xml"));
+        // Spider Man
+        JSONObject Spider = new JSONObject();
+        Charlie.put("prenom", "Spider");
+        Charlie.put("nom",    "Man");
+        Charlie.put("eamil",  "spider@man.movie");
 
-            //prologue
-            document.setXmlStandalone(true);
-            document.setXmlVersion("1.1");
-            transformer.setOutputProperty(OutputKeys.ENCODING,   "UTF-8");
+        JSONObject SpiderObject = new JSONObject();
+        SpiderObject.put("personne", Charlie);
 
-            //formatage
-            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
-            transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2");
+        // Ajouter les personnes à la liste
+        JSONArray personneList = new JSONArray();
+        personneList.add(CharlieObject);
+        personneList.add(SpiderObject);
 
-            //sortie
-            transformer.transform(source, sortie);
-        }
-        catch (final ParserConfigurationException e) {
-            e.printStackTrace();
-        }
-        catch (TransformerConfigurationException e) {
-            e.printStackTrace();
-        }
-        catch (TransformerException e) {
+        // Ecriture du fichier JSON
+        try (FileWriter file = new FileWriter("src/jsonFile.json")) {
+
+            file.write(personneList.toJSONString());
+            file.flush();
+
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
