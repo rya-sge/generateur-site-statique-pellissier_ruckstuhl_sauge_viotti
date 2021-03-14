aa8750b919869691527afa5d0fdf7578d1895986
rya-sge <ryan.sauge@heig-vd.ch>
2021-03-14 14:24:08 +0100

    Ajout cmd init + test basique

---
+++ src/main/java/Init.java
@@ -0,0 +1,46 @@
+/*
+Date : 05.03.2021
+Groupe : PRSV
+Description : Implémentation cmd Init
+ */
+
+import picocli.CommandLine;
+import utils.Contenu;
+import utils.Utils;
+
+import java.io.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.LinkedList;
+import java.util.concurrent.Callable;
+
+
+@CommandLine.Command(name = "init", description = "Initialiser un site static")
+public class Init implements Callable<Integer> {
+    //@CommandLine.Option(names = {"-u", "--user"}, description = "User name")
+    @CommandLine.Parameters(paramLabel = "<rootDirectory>", description = "Dossier root")
+    private String rootDirectory;
+    /**
+     * Source : http://www.codeurjava.com/2015/07/java-obtenir-la-date-et-heure-courante-avec-date-et-calendar.html
+     */
+    @Override
+    public Integer call() {
+        System.out.println("Initialisation du site static");
+        String name = "index.md";
+        DateFormat format = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+        Calendar calendar = Calendar.getInstance();
+        String titre = "Mon premier article";
+        String date = format.format(calendar.getTime());
+        LinkedList<String> c = new LinkedList();
+        c.add("# Bienvenue vers le meilleur generateur");
+        c.add("## Page d'acceuil");
+        c.add("### Votre contenu");
+        Contenu contenu = new Contenu(titre, "", date, c);
+        Utils.createFile(contenu.toString(), name);
+        return 1;
+    }
+
+}
+
+
