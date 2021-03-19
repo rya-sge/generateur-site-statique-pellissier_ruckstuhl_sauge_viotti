886f8a5f068cfc1df0a3e58e9cd87f61825c468e
Nico <n.viotti@hotmail.com>
2021-03-19 12:42:19 +0100

    Basic conversion and copy of files, not tested

--- src/main/java/Build.java
+++ src/main/java/Build.java
@@ -4,19 +4,67 @@ Groupe : PRSV
 Description : Implémentation cmd Build
  */
 
+import org.tautua.markdownpapers.Markdown;
+import org.tautua.markdownpapers.parser.ParseException;
 import picocli.CommandLine;
+
+import java.io.*;
+import java.nio.file.*;
+import java.util.concurrent.Callable;
 
 @CommandLine.Command(
         name = "Build",
         description = "Build"
 )
-public class Build implements Runnable {
-    @CommandLine.Parameters(paramLabel = "<nom>", description = "nom du truc à créer")
-    private String nom;
+public class Build implements Callable<Integer> {
+    @CommandLine.Parameters(paramLabel = "<rootDirectory>", description = "Dossier root")
+    private String rootDirectory;
 
     @Override
-    public void run() {
-        System.out.printf("%s : is that what you wanted, Sir ?", nom);
+    public Integer call() throws IOException, ParseException {
+        //Lecture du fichier md pour connaitre le site et le nom de domaine
+
+        try {
+
+            Path path = Paths.get(rootDirectory+"build");
+
+            //java.nio.file.Files;
+            Files.createDirectories(path);
+
+        } catch (IOException e) {
+
+            System.err.println("Failed to create directory : " + e.getMessage());
+        }
+
+        File folder = new File(rootDirectory+"/Dossier/");
+        File[] listofFiles = folder.listFiles();
+        for(File file : listofFiles) //Parcourt de tous les fichiers présents dans le dossier
+        {
+            if(file.isFile())
+            {
+                String filename = file.getName();
+                int index = filename.lastIndexOf('.');
+                if(index > 0) {
+                    String extension = filename.substring(index + 1);
+                    if(extension=="md") //Si le fichier est sous format md, il est convertit en html
+                    {
+                        Reader in = new FileReader(rootDirectory+"/dossier/"+filename);
+                        filename.replace("md","html");
+                        Writer out = new FileWriter(rootDirectory+"/build/dossier/"+ filename);
+                        Markdown md = new Markdown();
+                        md.transform(in, out);
+                    }
+                    else //Sinon copie le fichier sans l'altérer
+                    {
+                        Files.copy(file.toPath(),(new File(rootDirectory+"/build/dossier" + file.getName())).toPath());
+                    }
+                }
+            }
+
+        }
+
+
+        return null;
     }
 
 }
