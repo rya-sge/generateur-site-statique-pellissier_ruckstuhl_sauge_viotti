890005cd09a34e40aae358a4b2529c3a612f455b
david-pellissier <dav.pellissier@gmail.com>
2021-03-26 12:56:04 +0100

    OK La génération de package est prête. Par contre il y a un warning quand on fait "mvn package", mais ce n'est pas grave

--- src/main/java/utils/Version.java
+++ src/main/java/utils/Version.java
@@ -7,6 +7,7 @@ public class Version implements IVersionProvider {
     public String[] getVersion() throws Exception{
         String currentVersion = this.getClass().getPackage().getImplementationVersion();
         String appName = this.getClass().getPackage().getImplementationTitle();
-        return new String[]{currentVersion};
+        String strVersion = String.format("%s %s", appName, currentVersion);
+        return new String[]{strVersion};
     }
 }
