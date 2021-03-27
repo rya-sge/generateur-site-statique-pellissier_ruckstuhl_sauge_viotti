225037cbcb3a31bf77b69b275aac43c97a901f61
MichaelRuckstuhl <michael.ruckstuhl@heig-vd.ch>
2021-03-27 15:35:31 +0100

    Clean, éléments iunutiles supprimés. CleanTest vérifie que le dossier Build est supprimé et les dossiers tests sont créés dans src/test/java/utils/testfiles/

--- src/test/java/CleanTest.java
+++ src/test/java/CleanTest.java
@@ -6,18 +6,22 @@ Description : Vérifie les valeurs de retour de Clean,java
 
 import org.junit.jupiter.api.Test;
 import picocli.CommandLine;
+import utils.FileHandler;
 
 import java.io.File;
 import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 
 class CleanTest {
 
+    private final String testFilesPath =  "src/test/java/utils/testfiles/";
+
     @Test
     void cleanCallReturn0IfRootDoesNotExist(){
-        String rootName = "notExist";
+        String rootName = testFilesPath + "notExist";
         Clean c = new Clean();
         Integer val = new CommandLine(c).execute(rootName);
 
@@ -26,7 +30,7 @@ class CleanTest {
 
     @Test
     void cleanCallReturn0IfFolderEmpty(){
-        String rootName = "emptyFolder";
+        String rootName = testFilesPath + "emptyFolder";
         File root = new File(rootName);
         root.mkdir();
         Clean c = new Clean();
@@ -38,7 +42,7 @@ class CleanTest {
     @Test
     void cleanCallReturn0IfFolderDoesNotContainsBuild(){
 
-        String rootName = "noBuild";
+        String rootName = testFilesPath + "noBuild";
         File root = new File(rootName);
         root.mkdir();
 
@@ -67,7 +71,7 @@ class CleanTest {
     @Test
     void cleanCallReturn1IfFolderContainsBuild(){
 
-        String rootName = "withBuild";
+        String rootName = testFilesPath + "withBuild";
         File root = new File(rootName);
         root.mkdir();
 
@@ -106,5 +110,49 @@ class CleanTest {
         Integer val = new CommandLine(c).execute(rootName);
 
         assertEquals(val, 1);
+    }
+
+    @Test
+    void cleanCallEraseBuild(){
+
+        String rootName = testFilesPath + "eraseBuild";
+        File root = new File(rootName);
+        root.mkdir();
+
+        String subRoot = rootName + "/subDir";
+        String subRoot2 = rootName + "/subDir2";
+        String build = rootName + "/build";
+        String subBuild = build + "/subBuild";
+        String file1 = rootName + "/file1.txt";
+        String file2 = subRoot + "/file2.txt";
+        String file3 = build + "/file3.txt";
+        String file4 = subBuild + "/file4.txt";
+
+        File subRootF = new File(subRoot);
+        File subRoot2F = new File(subRoot2);
+        File buildF = new File(build);
+        File subBuildF = new File(subBuild);
+        File file1F = new File(file1);
+        File file2F = new File(file2);
+        File file3F = new File(file3);
+        File file4F = new File(file4);
+
+        subRootF.mkdir();
+        subRoot2F.mkdir();
+        buildF.mkdir();
+        subBuildF.mkdir();
+        try {
+            file1F.createNewFile();
+            file2F.createNewFile();
+            file3F.createNewFile();
+            file4F.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Clean c = new Clean();
+        Integer val = new CommandLine(c).execute(rootName);
+
+        assertFalse(FileHandler.exists(build));
     }
 }
