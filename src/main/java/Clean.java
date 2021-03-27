a2fea65512e881950923c2663fd8b5e7c80a7126
MichaelRuckstuhl <michael.ruckstuhl@heig-vd.ch>
2021-03-27 14:26:41 +0100

    Clean implémente callable à la place de runable. Test Junit checkent la valeur de retour de call

--- src/main/java/Clean.java
+++ src/main/java/Clean.java
@@ -5,55 +5,35 @@ Description : Implémentation cmd Clean
  */
 
 import picocli.CommandLine;
+import utils.FileHandler;
 
 import java.io.File;
+import java.util.concurrent.Callable;
 
 @CommandLine.Command(
         name = "clean",
         description = "nettoie le site statique"
 )
 
-class Clean implements C {
+class Clean implements Callable<Integer> {
 
     @CommandLine.Parameters(paramLabel = "<rootDirectory>", description = "Dossier root du site à effacer")
     private String rootDirectory;
 
-    /**
-     * Efface un dossier et son contenu.
-     * @param directory le dossier à effacer.
-     */
-    public void eraseNotEmptyDirectory(File directory){
-        File[] lFiles = directory.listFiles();
-        if (lFiles != null){
-            for (File file: lFiles){
-                if (file.isDirectory()){
-                    eraseNotEmptyDirectory(file);
-                }else {
-                    file.delete();
-                }
-            }
-        }
-        directory.delete();
-    }
-
     @Override
-    public void run() throws RuntimeException{
-
+    public Integer call() throws Exception {
         boolean buildNotFound = true;
         File root = new File(rootDirectory);
         File[] lFiles = root.listFiles();
-        if(lFiles != null){
-            for (File file: lFiles){
-                if (file.isDirectory() && file.getName().equals("build")){
-                    eraseNotEmptyDirectory(file);
+        if(lFiles != null) {
+            for (File file : lFiles) {
+                if (file.isDirectory() && file.getName().equals("build")) {
+                    FileHandler.eraseNotEmptyDirectory(file);
                     buildNotFound = false;
+                    return 1;
                 }
             }
-            if (buildNotFound){
-                throw new  RuntimeException("Dossier build non trouvé");
-            }
-        }else{
-            throw new  RuntimeException("Dossier vide");
         }
+        return 0;
     }
 }
