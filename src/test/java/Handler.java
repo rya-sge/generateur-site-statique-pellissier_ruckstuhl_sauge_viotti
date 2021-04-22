8bd94f170d41326f267b6328ef1ee4607b7b23c0
rya-sge <ryan.sauge@heig-vd.ch>
2021-04-22 19:17:48 +0200

    Test Handler Pas terminé, template

--- src/test/java/Handler.java
+++ src/test/java/Handler.java
@@ -1,16 +1,25 @@
 import global.ConstantesTest;
 import org.junit.Test;
 
-import static utils.FileHandler.eraseNotEmptyDirectory;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static utils.FileHandler.readFile;
 
 public class Handler {
     private final String testFilesPath =  ConstantesTest.TEST_FOLDER;
     @Test
     void test(){
-        String fileResultat  = "pageModel.html";
+
+        String fichierModele  = "pageModel.html";
         String fichierCree =  testFilesPath + "/page.html";
-        String content = readFile(fileResultat);
+
+        //Appel la fonction handler pour créer le fichier /page.html
+
+        //Lecture du contenu des fichiers crées
+        String fichierModeleContenu = readFile(fichierModele);
+        String fichierCreeContenu =  readFile(fichierCree);
+
+        //Le contenu entre le modele et le résutat doit être le même
+        assertEquals(fichierModeleContenu, fichierModeleContenu);
     }
 
 }
