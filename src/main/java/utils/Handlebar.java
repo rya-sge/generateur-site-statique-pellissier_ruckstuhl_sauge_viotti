e2ad100f0efbc01d8e0869bc74273ce348335627
rya-sge <ryan.sauge@heig-vd.ch>
2021-04-23 16:49:16 +0200

    handlebar essai

--- src/main/java/utils/Handlebar.java
+++ src/main/java/utils/Handlebar.java
@@ -1,14 +1,29 @@
 package utils;
 
 import com.github.jknack.handlebars.Handlebars;
+import com.github.jknack.handlebars.Template;
+import com.github.jknack.handlebars.io.ClassPathTemplateLoader;
+import com.github.jknack.handlebars.io.TemplateLoader;
+
+import java.io.IOException;
 
 public class Handlebar {
-
+    public final static String TEST_FOLDER = "./target/test/";
+    private final String testFilesPath =  TEST_FOLDER;
+    final private String rootDirectory = testFilesPath  + "root";
     void transform(){
-        Handlebars handlebars = new Handlebars();
 
-        Template template = handlebars.compileInline("Hello {{this}}!");
 
-        System.out.println(template.apply("Handlebars.java"));
+        TemplateLoader loader = new ClassPathTemplateLoader();
+        //loader.setPrefix(rootDirectory +"/template");
+        loader.setPrefix("");
+        Handlebars handlebars = new Handlebars(loader);
+
+        try{
+            Template template = handlebars.compile("mytemplate");
+            //System.out.println("hello" + template.apply("Handlebar.java"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ src/test/java/utils/HandlebarTest.java
@@ -0,0 +1,14 @@
+package utils;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HandlebarTest {
+
+    @Test
+    void transform() {
+        Handlebar b = new Handlebar();
+        b.transform();
+    }
+}
