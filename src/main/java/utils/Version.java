b582e45b5dc6e5390f5f699f43837fb20271512e
rya-sge <ryan.sauge@heig-vd.ch>
2021-05-07 19:08:02 +0200

    Ajout commentaire

--- src/main/java/utils/Version.java
+++ src/main/java/utils/Version.java
@@ -1,9 +1,20 @@
+/*
+Date : 27.03.2021
+Groupe : PRSV
+Description : Class permettant d'obtenir la version du projet.
+ */
+
 package utils;
 
 import picocli.CommandLine.IVersionProvider;
 
 public class Version implements IVersionProvider {
-    public String[] getVersion() throws Exception{
+
+    /**
+     *
+     * @return
+     */
+    public String[] getVersion(){
         String currentVersion = this.getClass().getPackage().getImplementationVersion();
         String appName = this.getClass().getPackage().getImplementationTitle();
         String strVersion = String.format("%s %s", appName, currentVersion);
