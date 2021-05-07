4ada233756bf8ac86faf5076a9f296bda93f3aad
david-pellissier <dav.pellissier@gmail.com>
2021-05-07 18:11:53 +0200

    Revert "Revert "Ft impl build""
    This reverts commit 81b38df7c11c1b57e9b3305903619deba88602dc.

--- src/test/java/BuildTest.java
+++ src/test/java/BuildTest.java
@@ -3,10 +3,12 @@ Date : 27.03.2021
 Groupe : PRSV
 Description : Test cmd Build
  */
+
 import global.ConstantesTest;
 import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.Test;
 import picocli.CommandLine;
+import utils.HandlerbarTest;
 
 import java.io.ByteArrayInputStream;
 import java.io.File;
@@ -19,18 +21,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class BuildTest {
-    private final String testFilesPath =  ConstantesTest.TEST_FOLDER;
-    final private String rootDirectory = testFilesPath  + "/BuildTest";
+    private final String testFilesPath = ConstantesTest.TEST_FOLDER;
+    final private String rootDirectory = testFilesPath + "/BuildTest";
 
-    @Test
-    void call() throws IOException {
-        List<File> listFiles = new ArrayList<>();
-        List<File> listDir = new ArrayList<>();
-        if(new File(rootDirectory).exists())
-        {
-            FileUtils.forceDelete(new File(rootDirectory));
-        }
 
+    void createArborescence() {
         final String titre = "My Poney Back";
         final String domaine = "Sparkle.com";
         final String description = "Un Lieu Magic où les poney vivent en paix et en harmonie";
@@ -42,16 +37,29 @@ public class BuildTest {
         System.setIn(in);
 
         new CommandLine(i).execute(rootDirectory);
+    }
+
+    @Test
+    void call() throws IOException {
+
+        List<File> listFiles = new ArrayList<>();
+        List<File> listDir = new ArrayList<>();
+        if (new File(rootDirectory).exists()) {
+            FileUtils.forceDelete(new File(rootDirectory));
+        }
+
+        createArborescence();
+
 
         //Suppression du dossier si existant
         File dir = new File(testFilesPath + "BuildTest");
+
         //Création des dossiers root, résultat final ciblé
 
-        listDir.add(new File(dir+"/build/content/"));
+        listDir.add(new File(dir + "/build/content/"));
         listDir.add(new File(dir + "/build/"));
 
-        for(File d : listDir)
-        {
+        for (File d : listDir) {
             FileUtils.forceMkdir(d);
         }
 
@@ -59,28 +67,30 @@ public class BuildTest {
         listFiles.add(new File(dir + "/build/content/image.png"));
         listFiles.add(new File(dir + "/build/index.html"));
 
-        for(File f : listFiles)
-        {
+        for (File f : listFiles) {
             f.createNewFile();
         }
 
         //Création du second dossier
         File dir2 = new File(testFilesPath + "BuildTest2");
-        if(dir2.exists())
-        {
+        if (dir2.exists()) {
             FileUtils.forceDelete(dir2);
         }
 
+
         //Création des dossiers sur lesquels seront lancé la commande Build
-        FileUtils.copyDirectory(dir,dir2);
-        File dir2Build = new File (dir2+"/build");
-        if(dir2Build.exists())
-        {
+        FileUtils.copyDirectory(dir, dir2);
+        File dir2Build = new File(dir2 + "/build");
+        if (dir2Build.exists()) {
             FileUtils.forceDelete(dir2Build);
         }
 
+        //Test handlebar
+        HandlerbarTest ht = new HandlerbarTest();
+        ht.test();
+
         Build b = new Build();
-        assertEquals(1,new CommandLine(b).execute(dir2.toString()));
+        assertEquals(1, new CommandLine(b).execute(dir2.toString()));
 
         //Préparation des tests
         File[] fileList = dir.listFiles();
@@ -94,13 +104,11 @@ public class BuildTest {
         File idxCreated = new File(testFilesPath + "BuildTest2/build/index.html");
         assertEquals(idxBase.exists(), idxCreated.exists());
 
-        if(dir.exists())
-        {
-        //   FileUtils.forceDelete(dir);
+        if (dir.exists()) {
+            //   FileUtils.forceDelete(dir);
         }
-        if(dir2.exists())
-        {
-        //    FileUtils.forceDelete(dir2);
+        if (dir2.exists()) {
+            //    FileUtils.forceDelete(dir2);
         }
 
     }
