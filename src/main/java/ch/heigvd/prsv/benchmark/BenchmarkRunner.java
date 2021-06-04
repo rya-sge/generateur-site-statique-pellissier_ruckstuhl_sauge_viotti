cfb1e639e375ad935a0058ab01a5d0d6ef5086cf
rya-sge <ryan.sauge@heig-vd.ch>
2021-06-04 17:23:04 +0200

    Ajout commentaire

--- src/main/java/ch/heigvd/prsv/benchmark/BenchmarkRunner.java
+++ src/main/java/ch/heigvd/prsv/benchmark/BenchmarkRunner.java
@@ -1,4 +1,10 @@
 package ch.heigvd.prsv.benchmark;
+
+/*
+Date : 04.06.2021
+Groupe : PRSV
+Description : Effectuer un benchmark avec JMH
+ */
 import ch.heigvd.prsv.utils.FileHandler;
 import ch.heigvd.prsv.Build;
 import ch.heigvd.prsv.Init;
@@ -11,10 +17,20 @@ import java.io.File;
 
 public class BenchmarkRunner {
     static String dir = "./benchmark";
+
+    /**
+     * Changer selon le mode souhaité
+     */
     @Benchmark @BenchmarkMode(Mode.AverageTime)
     public void init() {
         new CommandLine(new Build()).execute(dir.toString());
     }
+
+    /**
+     * Permet de lancer le benchmark de façon indépendante au reste de l'application
+     * @param args
+     * @throws Exception
+     */
     public static void main(String[] args) throws Exception {
         File dossier = new File(dir);
         FileHandler.eraseNotEmptyDirectory(dossier);
