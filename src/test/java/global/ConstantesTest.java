838db98c100e824db48d8b8865566e09c753770f
david-pellissier <dav.pellissier@gmail.com>
2021-06-04 12:14:55 +0200

    changement d'une constante

--- src/test/java/global/ConstantesTest.java
+++ src/test/java/global/ConstantesTest.java
@@ -1,5 +1,5 @@
 package global;
 
 public class ConstantesTest {
-    public final static String TEST_FOLDER = "/run/media/david/Cours/BA4_GEN/labo/projet-pellissier_ruckstuhl_sauge_viotti-prsv/target/test/";
+    public final static String TEST_FOLDER = "./target/test/";
 }
